438834d42a25fa5a34347109cfbb68d5ae8c7d4d
Yoann Fleury <yoann.fleury@yahoo.com>
2022-06-28 14:08:37 +0200

    feat: remove linkedin and improve image support

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 
 type HomeProps = {
@@ -10,19 +11,19 @@ const Home: NextPage<HomeProps> = ({ hours }) => {
   const containerStyles = {
     sunrise: {
       hours: [5, 11],
-      style: styles.containerSunset,
+      src: "/sunset.png",
     },
     day: {
       hours: [11, 17],
-      style: styles.containerDay,
+      src: "/day.png",
     },
     sunset: {
       hours: [17, 22],
-      style: styles.containerSunset,
+      src: "/sunset.png",
     },
     night: {
       hours: [22, 5],
-      style: styles.containerNight,
+      src: "/night.png",
     },
   };
 
@@ -38,7 +39,7 @@ const Home: NextPage<HomeProps> = ({ hours }) => {
   };
 
   return (
-    <div className={getContainerStyle()?.style}>
+    <main>
       <Head>
         <title>Yoann Fleury - Web Developer</title>
         <meta
@@ -47,70 +48,74 @@ const Home: NextPage<HomeProps> = ({ hours }) => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.card}>
-        <p>Hello! Bonjour !</p>
-        <p>
-          My name is <strong>Yoann Fleury</strong>, I am a frontend web
-          developer at{" "}
-          <a
-            href="https://www.bearstudio.fr"
-            title="BearStudio's main page"
-            target="_blank"
-            rel="noreferrer"
-          >
-            BearStudio
-          </a>{" "}
-          and a volunteer at{" "}
-          <a
-            href="https://www.codeursenseine.com"
-            title="Codeurs en Seine website, a french association"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Codeurs en Seine
-          </a>
-        </p>
-        <p>
-          Follow me on{" "}
-          <a
-            href="https://twitter.com/yoannfleurydev"
-            title="Yoann Fleury's Twitter profile"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Twitter
-          </a>
-          ,{" "}
-          <a
-            href="https://github.com/yoannfleurydev"
-            title="Yoann Fleury's GitHub profile"
-            target="_blank"
-            rel="noreferrer"
-          >
-            GitHub
-          </a>
-          ,{" "}
-          <a
-            href="https://changelog.yoannfleury.dev"
-            title="Yoann Fleury's Polywork page"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Polywork
-          </a>
-          ,{" "}
-          <a
-            href="https://www.linkedin.com/in/yoannfleurydev/"
-            title="Yoann Fleury's LinkedIn profile"
-            target="_blank"
-            rel="noreferrer"
-          >
-            LinkedIn
-          </a>{" "}
-          or add me on Discord with the username <em>YoannFleuryDev#0001</em>
-        </p>
+      <div className={styles.image}>
+        <Image
+          src={getContainerStyle().src}
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          alt=""
+        />
       </div>
-    </div>
+
+      <div className={styles.cardWrapper}>
+        <div className={styles.card}>
+          <p>Hello! Bonjour !</p>
+          <p>
+            My name is <strong>Yoann Fleury</strong>, I am a frontend web
+            developer at{" "}
+            <a
+              href="https://www.bearstudio.fr"
+              title="BearStudio's main page"
+              target="_blank"
+              rel="noreferrer"
+            >
+              BearStudio
+            </a>{" "}
+            and a volunteer at{" "}
+            <a
+              href="https://www.codeursenseine.com"
+              title="Codeurs en Seine website, a french association"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Codeurs en Seine
+            </a>
+          </p>
+          <p>
+            Follow me on{" "}
+            <a
+              href="https://twitter.com/yoannfleurydev"
+              title="Yoann Fleury's Twitter profile"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Twitter
+            </a>
+            ,{" "}
+            <a
+              href="https://github.com/yoannfleurydev"
+              title="Yoann Fleury's GitHub profile"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+            ,{" "}
+            <a
+              href="https://changelog.yoannfleury.dev"
+              title="Yoann Fleury's Polywork page"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Polywork
+            </a>
+            , or add me on Discord with the username{" "}
+            <em>YoannFleuryDev#0001</em>
+          </p>
+        </div>
+      </div>
+    </main>
   );
 };
 
