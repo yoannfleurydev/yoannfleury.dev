cc4de5d1b13b31ac5cf5a45d59a87d404d39850e
Yoann Fleury <yoann.fleury@yahoo.com>
2024-07-08 23:40:29 +0200

    feat: new website theme
    more coming soon

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ["Sono Variable"],
+        sans: ["Work Sans Variable"],
+      },
+      colors: {
+        background: "#1d1f22",
+        primary: "#fafcfc",
+        secondary: "#fbbcd1",
+      },
+    },
   },
   plugins: [],
 };
